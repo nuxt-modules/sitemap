dfc4b1c33044143309eb7d8db015c3b1e2496d7d
Harlan Wilton <harlan@harlanzw.com>
2022-12-13 16:46:31 +1100

    fix: eventHandler error

--- src/runtime/dev-sitemap.ts
+++ src/runtime/dev-sitemap.ts
@@ -1,3 +1,4 @@
-export default eventHandler(async () => {
+import { defineEventHandler } from '#imports'
+export default defineEventHandler(async () => {
   return 'Sitemap is not available in development mode.<br><br>If you\'d like to view it run <code>nuxi generate</code> or <code>nuxi build</code>.'
 })
