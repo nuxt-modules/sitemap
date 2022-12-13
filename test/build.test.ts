a0e5fe195b6cb781e2a481f216c8a8e66f60fc06
Harlan Wilton <harlan@harlanzw.com>
2022-12-13 16:17:29 +1100

    chore: extend test times

--- test/build.test.ts
+++ test/build.test.ts
@@ -16,5 +16,5 @@ describe('build', () => {
 
     const sitemap = await $fetch('http://localhost:3000/sitemap.xml')
     expect(sitemap).toMatchInlineSnapshot('"<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?><urlset xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\" xmlns:news=\\"http://www.google.com/schemas/sitemap-news/0.9\\" xmlns:xhtml=\\"http://www.w3.org/1999/xhtml\\" xmlns:image=\\"http://www.google.com/schemas/sitemap-image/1.1\\" xmlns:video=\\"http://www.google.com/schemas/sitemap-video/1.1\\"><url><loc>https://example.com/</loc></url><url><loc>https://example.com/about</loc><changefreq>daily</changefreq><priority>0.3</priority></url><url><loc>https://example.com/hidden-path-but-in-sitemap</loc></url></urlset>"')
-  }, 30000)
+  }, 60000)
 })
--- test/generate.test.ts
+++ test/generate.test.ts
@@ -21,5 +21,5 @@ describe('generate', () => {
 
     const sitemap = await readFile(resolve(rootDir, '.output/public/sitemap.xml'), 'utf-8')
     expect(sitemap).toMatchInlineSnapshot('"<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?><urlset xmlns=\\"http://www.sitemaps.org/schemas/sitemap/0.9\\" xmlns:news=\\"http://www.google.com/schemas/sitemap-news/0.9\\" xmlns:xhtml=\\"http://www.w3.org/1999/xhtml\\" xmlns:image=\\"http://www.google.com/schemas/sitemap-image/1.1\\" xmlns:video=\\"http://www.google.com/schemas/sitemap-video/1.1\\"><url><loc>https://example.com/</loc></url><url><loc>https://example.com/about</loc><changefreq>daily</changefreq><priority>0.3</priority></url><url><loc>https://example.com/hidden-path-but-in-sitemap</loc></url></urlset>"')
-  }, 30000)
+  }, 60000)
 })
