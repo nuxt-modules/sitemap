ef9a397e026dfff1c16095effd3e0b507d6af123
Harlan Wilton <harlan@harlanzw.com>
2023-03-16 15:36:34 +1100

    test: implement better tests

---
+++ test/prod-slash.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from 'vitest'
+import { createResolver } from '@nuxt/kit'
+import { $fetch, setup } from '@nuxt/test-utils'
+
+const { resolve } = createResolver(import.meta.url)
+
+await setup({
+  rootDir: resolve('../.playground'),
+  build: true,
+  server: true,
+  nuxtConfig: {
+    sitemap: {
+      trailingSlash: true,
+      autoLastmod: false,
+      siteUrl: 'https://nuxt-simple-sitemap.com',
+    },
+  },
+})
+describe('prod slash', () => {
+  it('basic', async () => {
+    const posts = await $fetch('/posts-sitemap.xml')
+    // extract the URLs from loc using regex
+    const postUrls = posts.match(/<loc>(.*?)<\/loc>/g)!.map(url => url.replace(/<\/?loc>/g, ''))
+    postUrls.forEach((url) => {
+      expect(url.endsWith('/')).toBeTruthy()
+    })
+
+    const pages = await $fetch('/posts-sitemap.xml')
+    // extract the URLs from loc using regex
+    const pageUrls = pages.match(/<loc>(.*?)<\/loc>/g)!.map(url => url.replace(/<\/?loc>/g, ''))
+    pageUrls.forEach((url) => {
+      expect(url.endsWith('/')).toBeTruthy()
+    })
+  }, 60000)
+})
