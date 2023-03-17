4b793f6c52be3a0aea782fdfa01c3f52ae57726c
Harlan Wilton <harlan@harlanzw.com>
2023-03-17 13:07:06 +1100

    fix(document-driven): strip /index and content prefix

--- src/runtime/routes/document-driven-urls.ts
+++ src/runtime/routes/document-driven-urls.ts
@@ -2,9 +2,10 @@ import { defineEventHandler } from 'h3'
 import type { ResolvedSitemapEntry } from '../../types'
 import { useStorage } from '#imports'
 
-export default defineEventHandler(async (e) => {
+export default defineEventHandler(async () => {
+  const prefix = 'cache:content:parsed:content'
   // create route from document driven mode
-  const parsedKeys = (await useStorage().getKeys('cache:content:parsed'))
+  const parsedKeys = (await useStorage().getKeys(prefix))
     .filter(k => k.endsWith('.md') && !k.includes('/_'))
   const urls: ResolvedSitemapEntry = []
   for (const k of parsedKeys) {
@@ -16,11 +17,12 @@ export default defineEventHandler(async (e) => {
       .map(i => ({
         loc: i.props.src,
       })) || []
-    const loc = k.replace('cache:content:parsed', '')
+    const loc = k.replace(prefix, '')
       .replaceAll(':', '/')
       // need to strip out the leading number such as 0.index.md -> index.md
       .replace(/\/\d+\./, '/')
       .split('.')[0]
+      .replace('/index', '')
     urls.push({ loc, lastmod: meta?.mtime, images })
   }
   return urls
