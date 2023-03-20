5d198b705fd03a360d69f2456842d74c12da3dee
Harlan Wilton <harlan@harlanzw.com>
2023-03-21 03:37:14 +1100

    fix: use temp redirect for dev sitemap_index.xml

--- src/runtime/routes/sitemap.xml.ts
+++ src/runtime/routes/sitemap.xml.ts
@@ -9,8 +9,8 @@ export default defineEventHandler(async (e) => {
   const sitemapConfig = useRuntimeConfig()['nuxt-simple-sitemap']
   // we need to check if we're rendering multiple sitemaps from the index sitemap
   if (sitemapConfig.sitemaps) {
-    // redirect to sitemap_index.xml
-    return sendRedirect(e, '/sitemap_index.xml', 301)
+    // redirect to sitemap_index.xml (302 in dev to avoid caching issues)
+    return sendRedirect(e, '/sitemap_index.xml', process.dev ? 302 : 301)
   }
 
   // need to clone the config object to make it writable
