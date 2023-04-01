68e6e251e5861745300a0e012f2faadebb57b4c0
Harlan Wilton <harlan@harlanzw.com>
2023-04-02 02:03:03 +0700

    chore: fix build

--- src/runtime/routes/document-driven-urls.ts
+++ src/runtime/routes/document-driven-urls.ts
@@ -3,6 +3,8 @@ import type { ResolvedSitemapEntry } from '../../types'
 import { useStorage, useRuntimeConfig } from '#imports'
 
 export default defineEventHandler(async () => {
+  const sitemapConfig = useRuntimeConfig()['nuxt-simple-sitemap']
+
   const prefix = 'cache:content:parsed:content'
   // create route from document driven mode
   const parsedKeys = (await useStorage().getKeys(prefix))
@@ -13,11 +15,14 @@ export default defineEventHandler(async () => {
     const meta = await useStorage().getMeta(k.replace(prefix, 'content:source:content'))
     const item = await useStorage().getItem(k)
     // add any top level images
-    const images = useRuntimeConfig()['nuxt-simple-sitemap'].discoverImages ? (item?.parsed.body?.children
-      ?.filter(c => ['image', 'img', 'nuxtimg', 'nuxt-img'].includes(c.tag?.toLowerCase()) && c.props?.src)
-      .map(i => ({
-        loc: i.props.src,
-      })) || []) || [];
+    let images = []
+    if (sitemapConfig.discoverImages) {
+      images = (item?.parsed.body?.children
+        ?.filter(c => ['image', 'img', 'nuxtimg', 'nuxt-img'].includes(c.tag?.toLowerCase()) && c.props?.src)
+        .map(i => ({
+          loc: i.props.src,
+        })) || [])
+    }
     const loc = k.replace(prefix, '')
       .replaceAll(':', '/')
       // need to strip out the leading number such as 0.index.md -> index.md
