f148b1618c676249bfe754114f2cb084756bb243
Harlan Wilton <harlan@harlanzw.com>
2023-05-20 11:08:37 +0700

    fix: use x-forward headers for host resolution
    Fixe #53
    Fixes #55

--- src/runtime/util/nuxt.ts
+++ src/runtime/util/nuxt.ts
@@ -5,8 +5,8 @@ import { useRuntimeConfig } from '#imports'
 
 export function useHostname(e: H3Event) {
   const base = useRuntimeConfig().app.baseURL
-  const host = getRequestHost(e) || process.env.NITRO_HOST || process.env.HOST || 'localhost'
-  const protocol = getRequestProtocol(e)
+  const host = getRequestHost(e, { xForwardedHost: true }) || process.env.NITRO_HOST || process.env.HOST || 'localhost'
+  const protocol = getRequestProtocol(e, { xForwardedProto: true })
   const useHttp = process.env.NODE_ENV === 'development' || host.includes('127.0.0.1') || host.includes('localhost') || protocol === 'http'
   let port = host.includes(':') ? host.split(':').pop() : false
   // try and avoid adding port if not needed, mainly needed for dev and prerendering
