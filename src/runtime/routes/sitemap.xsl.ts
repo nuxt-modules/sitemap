927d40e2e500dd4c495e6d8caf579bcd70e4d6da
Harlan Wilton <harlan@harlanzw.com>
2023-09-14 11:58:59 +0200

    fix: add google `news` sitemap namespace

--- src/runtime/routes/sitemap.xsl.ts
+++ src/runtime/routes/sitemap.xsl.ts
@@ -56,6 +56,7 @@ export default defineEventHandler(async (e) => {
                 xmlns:html="http://www.w3.org/TR/REC-html40"
                 xmlns:image="http://www.google.com/schemas/sitemap-image/1.1"
                 xmlns:sitemap="http://www.sitemaps.org/schemas/sitemap/0.9"
+                xmlns:news="http://www.google.com/schemas/sitemap-news/0.9"
                 xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
   <xsl:output method="html" version="1.0" encoding="UTF-8" indent="yes"/>
   <xsl:template match="/">
