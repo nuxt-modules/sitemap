07e73e2c793ec36c508f4a2d01282d412d405577
Pablo <pablo@gutierrezbarcelo.com>
2023-12-10 13:29:09 +0100

    chore: update playground

--- .playground/nuxt.config.ts
+++ .playground/nuxt.config.ts
@@ -112,7 +112,7 @@ export default defineNuxtConfig({
           '/api/sitemap-foo',
           'https://example.com/invalid.json',
         ],
-        exclude: ['/en/blog/**', '/fr/blog/**', '/blog/**', { regex: '/.*hide-me.*/g' }, { regex: '/.*hide-me.*/' }],
+        exclude: ['/en/blog/**', '/fr/blog/**', '/blog/**', /.*hide-me.*/g],
         urls: [
           {
             loc: '/about',
