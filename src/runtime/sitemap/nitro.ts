c290df26f225113ca21f6915df914f9527dada9d
Harlan Wilton <harlan@harlanzw.com>
2023-12-12 15:57:05 +1100

    fix: prerendering `sitemap_index.xml` error

--- src/runtime/sitemap/nitro.ts
+++ src/runtime/sitemap/nitro.ts
@@ -1,15 +1,17 @@
 import { type H3Event, getQuery, setHeader } from 'h3'
 import { fixSlashes } from 'site-config-stack'
-import type { SitemapDefinition } from '../types'
+import type { NitroUrlResolvers, SitemapDefinition } from '../types'
 import { buildSitemap } from './builder/sitemap'
 import { buildSitemapIndex } from './builder/sitemap-index'
-import { createSitePathResolver, useNitroApp, useSiteConfig } from '#imports'
+import { useNitroApp } from '#internal/nitro'
+import { createSitePathResolver, useSiteConfig } from '#internal/nuxt-site-config'
 
-export function useNitroUrlResolvers(e: H3Event) {
+export function useNitroUrlResolvers(e: H3Event): NitroUrlResolvers {
   const canonicalQuery = getQuery(e).canonical
   const isShowingCanonical = typeof canonicalQuery !== 'undefined' && canonicalQuery !== 'false'
   const siteConfig = useSiteConfig(e)
   return {
+    event: e,
     fixSlashes: (path: string) => fixSlashes(siteConfig.trailingSlash, path),
     // we need these as they depend on the nitro event
     canonicalUrlResolver: createSitePathResolver(e, {
