d5de35373afdbee84bed62d3c9b54773817abde1
Ray Blair <rayblair06@hotmail.com>
2024-06-28 04:25:06 +0100

    test: additional extractSitemapMetaFromHtml coverage (#296)

--- test/unit/extractSitemapMetaFromHtml.test.ts
+++ test/unit/extractSitemapMetaFromHtml.test.ts
@@ -28,4 +28,59 @@ describe('extractSitemapMetaFromHtml', () => {
       }
     `)
   })
+
+  it('extracts images from HTML', async () => {
+    const mainTag = '<main>'
+    const mainClosingTag = '</main>'
+    const discoverableImageHTML = `
+      <img
+        src="https://res.cloudinary.com/dl6o1xpyq/image/upload/f_jpg,q_auto:best,dpr_auto,w_240,h_240/images/harlan-wilton"
+        alt="Harlan Wilton"
+      />
+    `
+    const excludeImageDataHTML = `
+      <img
+        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAA..."
+      />
+    `
+    
+    const excludeImageBlobHTML = `
+      <img
+        src="blob:http://example.com/12345678-1234-5678-1234-567812345678"
+      />
+    `
+    const excludeImageFileHTML = `
+      <img
+        src="file:///C:/path/to/image.jpg"
+      />
+    `
+
+    // Test case 1 - Single discoverable image
+    const html1 = `${mainTag}${discoverableImageHTML}${mainClosingTag}`
+    const testcase1 = extractSitemapMetaFromHtml(html1)
+
+    expect(testcase1).toMatchInlineSnapshot(`
+      {
+        "images": [
+          {
+            "loc": "https://res.cloudinary.com/dl6o1xpyq/image/upload/f_jpg,q_auto:best,dpr_auto,w_240,h_240/images/harlan-wilton",
+          },
+        ],
+      }
+    `)
+
+    // Test case 2 - Single discoverable image with excluded image values
+    const html2 = `${mainTag}${discoverableImageHTML}${excludeImageDataHTML}${excludeImageBlobHTML}${excludeImageFileHTML}${mainClosingTag}`
+    const testcase2 = extractSitemapMetaFromHtml(html2)
+
+    expect(testcase2).toMatchInlineSnapshot(`
+      {
+        "images": [
+          {
+            "loc": "https://res.cloudinary.com/dl6o1xpyq/image/upload/f_jpg,q_auto:best,dpr_auto,w_240,h_240/images/harlan-wilton",
+          },
+        ],
+      }
+    `)
+  })
 })
