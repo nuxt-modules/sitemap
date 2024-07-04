3a824133c17fc24ef9a60bf337d0dac9c7f8cbeb
n0099 <n@n0099.net>
2024-07-04 12:40:57 +0000

    Update sitemap-index.ts

--- src/runtime/nitro/sitemap/builder/sitemap-index.ts
+++ src/runtime/nitro/sitemap/builder/sitemap-index.ts
@@ -123,7 +123,7 @@ export async function buildSitemapIndex(resolvers: NitroUrlResolvers, runtimeCon
   ].filter(Boolean).join('\n')).join('\n')
 
   return wrapSitemapXml([
-    '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    '<sitemapindex xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/siteindex.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
     sitemapXml,
     '</sitemapindex>',
   ], resolvers, { version, xsl, credits })
