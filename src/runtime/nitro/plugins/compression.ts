a6ec3c75fb69a7acad31f83d823de836cab35afa
David Frings, aka DevOpsDavid <dev@davidfrings.dev>
2024-11-08 17:30:47 +0000

    fix: Pull request for #380

--- src/runtime/nitro/plugins/compression.ts
+++ src/runtime/nitro/plugins/compression.ts
@@ -1,9 +1,9 @@
-import { useCompressionStream } from 'h3-compression'
+import { useCompression } from 'h3-compression'
 import { defineNitroPlugin } from '#imports'
 
 export default defineNitroPlugin((nitro) => {
   nitro.hooks.hook('beforeResponse', async (event, response) => {
     if (event.context._isSitemap)
-      await useCompressionStream(event, response)
+      await useCompression(event, response)
   })
 })
