2f176293c116534dd25b8c373f943d53e3ce9186
Cole Gentry <peapod2007@gmail.com>
2025-06-12 16:27:14 -0400

    Fixes sitemap to support newest version of nuxt-content
    Signed-off-by: Cole Gentry <peapod2007@gmail.com>

--- src/runtime/server/routes/__sitemap__/nuxt-content-urls-v3.ts
+++ src/runtime/server/routes/__sitemap__/nuxt-content-urls-v3.ts
@@ -1,34 +1,39 @@
-import { defineEventHandler } from 'h3'
+import { defineEventHandler } from "h3";
 // @ts-expect-error alias
-import { queryCollectionWithEvent } from '#sitemap/content-v3-nitro-path'
+import { queryCollection } from "@nuxt/content/nitro";
 // @ts-expect-error alias
-import manifest from '#content/manifest'
+import manifest from "#content/manifest";
 
 export default defineEventHandler(async (e) => {
-  const collections = []
+  const collections = [];
   // each collection in the manifest has a key => with fields which has a `sitemap`, we want to get all those
   for (const collection in manifest) {
     if (manifest[collection].fields.sitemap) {
-      collections.push(collection)
+      collections.push(collection);
     }
   }
   // now we need to handle multiple queries here, we want to run the requests in parralel
-  const contentList = []
+  const contentList = [];
   for (const collection of collections) {
-    contentList.push(queryCollectionWithEvent(e, collection).select('path', 'sitemap')
-      .where('path', 'IS NOT NULL')
-      .where('sitemap', 'IS NOT NULL')
-      .all())
+    contentList.push(
+      queryCollection(e, collection)
+        .select("path", "sitemap")
+        .where("path", "IS NOT NULL")
+        .where("sitemap", "IS NOT NULL")
+        .all()
+    );
   }
   // we need to wait for all the queries to finish
-  const results = await Promise.all(contentList)
+  const results = await Promise.all(contentList);
   // we need to flatten the results
-  return results.flatMap((c) => {
-    return c
-      .filter(c => c.sitemap !== false && c.path)
-      .flatMap(c => ({
-        loc: c.path,
-        ...(c.sitemap || {}),
-      }))
-  }).filter(Boolean)
-})
+  return results
+    .flatMap((c) => {
+      return c
+        .filter((c) => c.sitemap !== false && c.path)
+        .flatMap((c) => ({
+          loc: c.path,
+          ...(c.sitemap || {}),
+        }));
+    })
+    .filter(Boolean);
+});
