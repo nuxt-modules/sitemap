2d3cd70d9aa8a7d095b52c634cc218363a971da3
silverbackdan <daniel@silverback.is>
2025-10-01 15:16:19 +0100

    Fix schema locations to http

--- src/runtime/server/sitemap/builder/xml.ts
+++ src/runtime/server/sitemap/builder/xml.ts
@@ -19,7 +19,7 @@ export function escapeValueForXml(value: boolean | string | number): string {
 }
 
 // Cache constant strings to avoid repeated concatenation
-const URLSET_OPENING_TAG = '<urlset xmlns:xsi="https://www.w3.org/2001/XMLSchema-instance" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xsi:schemaLocation="https://www.sitemaps.org/schemas/sitemap/0.9 https://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd https://www.google.com/schemas/sitemap-image/1.1 https://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">'
+const URLSET_OPENING_TAG = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
 
 // Use a string builder approach for memory efficiency
 function buildUrlXml(url: ResolvedSitemapUrl): string {
