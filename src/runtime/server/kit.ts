bc5920452c5a453bf10409612c11173ef93b3612
Harlan Wilton <harlan@harlanzw.com>
2026-03-23 00:57:37 +1100

    fix: revert runtime/server/kit.ts to local impl to avoid nitropack resolution conflict

--- src/runtime/server/kit.ts
+++ src/runtime/server/kit.ts
@@ -1,1 +1,29 @@
-export { createNitroRouteRuleMatcher, withoutQuery } from 'nuxtseo-shared/runtime/server/kit'
+import type { NitroRouteRules } from 'nitropack'
+import { defu } from 'defu'
+import { useRuntimeConfig } from 'nitropack/runtime'
+import { createRouter as createRadixRouter, toRouteMatcher } from 'radix3'
+import { parseURL, withoutBase, withoutTrailingSlash } from 'ufo'
+
+export function withoutQuery(path: string) {
+  return path.split('?')[0]
+}
+
+export function createNitroRouteRuleMatcher() {
+  const { nitro, app } = useRuntimeConfig()
+  const _routeRulesMatcher = toRouteMatcher(
+    createRadixRouter({
+      routes: Object.fromEntries(
+        Object.entries(nitro?.routeRules || {})
+          .map(([path, rules]) => [path === '/' ? path : withoutTrailingSlash(path), rules]),
+      ),
+    }),
+  )
+  return (pathOrUrl: string) => {
+    const path = pathOrUrl[0] === '/' ? pathOrUrl : parseURL(pathOrUrl, app.baseURL).pathname
+    const pathWithoutQuery = withoutQuery(path)
+    return defu({}, ..._routeRulesMatcher.matchAll(
+      // radix3 does not support trailing slashes
+      withoutBase(pathWithoutQuery === '/' ? pathWithoutQuery : withoutTrailingSlash(pathWithoutQuery), app.baseURL),
+    ).reverse()) as NitroRouteRules
+  }
+}
