36719f3ef402e60645cedbb0918d4ad7737ba23c
Harlan Wilton <harlan@harlanzw.com>
2026-03-24 18:56:24 +1100

    chore: sync

--- devtools/nuxt.config.ts
+++ devtools/nuxt.config.ts
@@ -5,19 +5,9 @@ export default defineNuxtConfig({
 
   sitemap: false,
 
-  imports: {
-    autoImport: true,
-  },
-
   nitro: {
     prerender: {
-      routes: [
-        '/',
-        '/user-sources',
-        '/app-sources',
-        '/debug',
-        '/docs',
-      ],
+      routes: ['/', '/user-sources', '/app-sources', '/debug', '/docs'],
     },
     output: {
       publicDir: resolve(__dirname, '../dist/devtools'),
